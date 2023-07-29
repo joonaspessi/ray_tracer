500399a69ff0b1a8a8d3692c311cb80ec784e339
Joonas Pessi <joonas.pessi@kalmarglobal.com>
2023-07-29 14:25:25 +0300

    7_15

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,60 @@
+mod hit;
+mod ray;
+mod sphere;
 mod vec;
 
+use hit::{Hit, World};
+use ray::Ray;
+use sphere::Sphere;
 use std::io::{stderr, Write};
-use vec::{Color, Vec3};
+use vec::{Color, Point3, Vec3};
+
+fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
+    let oc = r.origin() - center;
+    let a = r.direction().length().powi(2);
+    let half_b = oc.dot(r.direction());
+    let c = oc.length().powi(2) - radius * radius;
+    let discrimant = half_b * half_b - a * c;
+
+    if discrimant < 0.0 {
+        -1.0
+    } else {
+        (-half_b - discrimant.sqrt()) / a
+    }
+}
+
+fn ray_color(r: &Ray, world: &World) -> Color {
+    if let Some(rec) = world.hit(r, 0.0, f64::INFINITY) {
+        0.5 * (rec.normal + Color::new(1.0, 1.0, 1.0))
+    } else {
+        let unit_direction = r.direction().normalized();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        // lerp
+        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
+}
 
 fn main() {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    // Image
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u64 = 256;
+    const IMAGE_HEIGHT: u64 = ((256 as f64) / ASPECT_RATIO) as u64;
+
+    // World
+    let mut world = World::new();
+    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
+    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = ASPECT_RATIO * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -16,11 +65,15 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let pixel_color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (j as f64) / ((IMAGE_HEIGHT - 1) as f64),
-                0.25,
+            let u = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
+            let v = (j as f64) / ((IMAGE_HEIGHT - 1) as f64);
+
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(&r, &world);
+
             println!("{}", pixel_color.format_color());
         }
     }
