66ffe36b0f621547d30a5c70a98e5253573cd4a0
Joonas Pessi <joonas.pessi@kalmarglobal.com>
2023-08-12 08:27:19 +0300

    Metal and dielectric materials

--- src/vec.rs
+++ src/vec.rs
@@ -99,6 +99,22 @@ impl Vec3 {
             (-1.0) * in_unit_sphere
         }
     }
+
+    pub fn near_zero(self) -> bool {
+        const EPS: f64 = 1.0e-8;
+        self[0].abs() < EPS && self[1].abs() < EPS && self[2].abs() < EPS
+    }
+
+    pub fn reflect(self, n: Vec3) -> Vec3 {
+        self - 2.0 * self.dot(n) * n
+    }
+
+    pub fn refract(self, n: Vec3, etai_over_etat: f64) -> Vec3 {
+        let cos_theta = ((-1.0) * self).dot(n).min(1.0);
+        let r_out_perp = etai_over_etat * (self + cos_theta * n);
+        let r_out_parallel = -(1.0 - r_out_perp.length().powi(2)).abs().sqrt() * n;
+        r_out_perp + r_out_parallel
+    }
 }
 
 impl Display for Vec3 {
@@ -163,6 +179,16 @@ impl Mul<f64> for Vec3 {
     fn mul(self, other: f64) -> Vec3 {
         Vec3 {
             e: [self[0] * other, self[1] * other, self[2] * other],
+        }
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [self[0] * other[0], self[1] * other[1], self[2] * other[2]],
         }
     }
 }
